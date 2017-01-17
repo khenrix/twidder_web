e1b976072a719162c309a11637e1b0f2001f74e1
Ruben Das <rubda680@student.liu.se>
2017-01-17 09:16:41 +0100

    Validating sign up information now.

--- client.js
+++ client.js
@@ -1,9 +1,26 @@
-displayView = function(){
+displayView = function(view){
     // the code required to display a view
+    document.getElementById("content").innerHTML = view.innerHTML;
 };
+
 window.onload = function(){
     //code that is executed as the page is loaded.
     //You shall put your own custom code here.
     //window.alert() is not allowed to be used in your implementation.
-    window.alert("Hello TDDD97!");
+    var welcome_view = document.getElementById("welcome_view");
+    displayView(welcome_view);
 };
+
+checkPassword = function(){
+    var pw_length = 5;
+    given_pw = document.getElementById("psw_reg").value;
+    given_pw_rep = document.getElementById("psw_reg_rep").value;
+    given_pw_length = given_pw.length;
+    console.log(given_pw_length);
+
+    if (given_pw === given_pw_rep && given_pw_length > pw_length){
+        return true;
+    }else{
+        window.alert("Given passwords must be the same and longer than 5 characters!")
+    }
+}
