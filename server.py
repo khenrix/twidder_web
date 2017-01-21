ee5cb7dd8c54f099f83f2976a5cbf2bf9c567c25
Ruben Das <rubda680@student.liu.se>
2017-01-21 14:09:27 +0100

    Added server.py and functions to be written in it.

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+def sign_in():
+    return None
+
+
+def sign_up():
+    return None
+
+
+def sign_out():
+    return None
+
+
+def change_password():
+    return None
+
+
+def get_user_data_by_token():
+    return None
+
+
+def get_user_data_by_email():
+    return None
+
+
+def get_user_messages_by_token():
+    return None
+
+
+def get_user_messages_by_email():
+    return None
+
+
+def post_message():
+    return None
