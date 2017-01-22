40cb90f16e618b29ebc08577f9d6b8f588f5fe71
Ruben Das <rubda680@student.liu.se>
2017-01-22 17:07:52 +0100

    Added app.routes for functions in server.py

--- server.py
+++ server.py
@@ -2,37 +2,46 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/sign-in', methods='POST')
 def sign_in():
     return None
 
 
+@app.route('/sign-up', methods='POST')
 def sign_up():
     return None
 
 
+@app.route('/sign-out', methods='POST')
 def sign_out():
     return None
 
 
+@app.route('/change-password', methods='POST')
 def change_password():
     return None
 
 
-def get_user_data_by_token():
+@app.route('/get-user-data-by-token/<token>', methods='GET')
+def get_user_data_by_token(token):
     return None
 
 
-def get_user_data_by_email():
+@app.route('/get-user-data-by-email/<email>', methods='GET')
+def get_user_data_by_email(email):
     return None
 
 
-def get_user_messages_by_token():
+@app.route('/get-user-messages-by-token/<token>', methods='GET')
+def get_user_messages_by_token(token):
     return None
 
 
-def get_user_messages_by_email():
+@app.route('/get-user-messages-by-email/<email>', methods='GET')
+def get_user_messages_by_email(email):
     return None
 
 
+@app.route('/post-message', methods='POST')
 def post_message():
     return None
